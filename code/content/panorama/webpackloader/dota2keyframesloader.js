a96f8c852c58617009aac54b575c697db46e745e
pipixia <741804121@qq.com>
2022-10-27 17:06:51 +0800

    111

--- code/content/panorama/webpackloader/dota2keyframesloader.js
+++ code/content/panorama/webpackloader/dota2keyframesloader.js
@@ -1,5 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 async function dota2KeyframesLoader(source) {
     source = source.replace(/@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g, (match, name) => {
         return match.replace(name, `'${name}'`);
